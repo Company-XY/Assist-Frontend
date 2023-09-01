0b878d4e368a4ef073de71f01634b92e098e3a2b
Olosko23 <oloogeorge633@gmail.com>
2023-09-01 17:44:54 +0300

    'newww'

--- src/pages/Dashboard/Client/postProject.jsx
+++ src/pages/Dashboard/Client/postProject.jsx
@@ -21,8 +21,8 @@ const PostProject = () => {
     switch (currentStep) {
       case 1:
         return (
-          <div>
-            <h2>Step 1: Project Title</h2>
+          <div className="flex flex-col justify-evenly h-fit">
+            <h2 className="font-normal text-xl">Step 1: Project Title</h2>
             <div className="flex flex-col">
               <label>Select Country</label>
               <select>
@@ -39,7 +39,12 @@ const PostProject = () => {
                 <option>Accounting</option>
               </select>
             </div>
-            <button onClick={handleNextStep}>Next</button>
+            <button
+              className="w-1/5 rounded-lg border-2 py-2 px-4 bg-purple-600 hover:bg-purple-800"
+              onClick={handleNextStep}
+            >
+              Next
+            </button>
           </div>
         );
       case 2:
@@ -67,8 +72,18 @@ const PostProject = () => {
                 placeholder="Details"
               />
             </div>
-            <button onClick={handlePrevStep}>Previous</button>
-            <button onClick={handleNextStep}>Next</button>
+            <button
+              className="w-1/5 rounded-lg border-2 py-2 px-4 bg-snow-100 hover:bg-purple-200"
+              onClick={handlePrevStep}
+            >
+              Previous
+            </button>
+            <button
+              className="w-1/5 rounded-lg border-2 py-2 px-4 bg-purple-600 hover:bg-purple-800"
+              onClick={handleNextStep}
+            >
+              Next
+            </button>
           </div>
         );
       case 3:
@@ -89,8 +104,18 @@ const PostProject = () => {
                 <option>Over 55000</option>
               </select>
             </div>
-            <button onClick={handlePrevStep}>Previous</button>
-            <button onClick={handleNextStep}>Next</button>
+            <button
+              className="w-1/5 rounded-lg border-2 py-2 px-4 bg-snow-100 hover:bg-purple-200"
+              onClick={handlePrevStep}
+            >
+              Previous
+            </button>
+            <button
+              className="w-1/5 rounded-lg border-2 py-2 px-4 bg-purple-600 hover:bg-purple-800"
+              onClick={handleNextStep}
+            >
+              Next
+            </button>
           </div>
         );
       case 4:
@@ -105,8 +130,18 @@ const PostProject = () => {
               <label>Upload any relevant files</label>
               <input type="file" />
             </div>{" "}
-            <button onClick={handlePrevStep}>Previous</button>
-            <button onClick={handleNextStep}>Next</button>
+            <button
+              className="w-1/5 rounded-lg border-2 py-2 px-4 bg-snow-100 hover:bg-purple-200"
+              onClick={handlePrevStep}
+            >
+              Previous
+            </button>
+            <button
+              className="w-1/5 rounded-lg border-2 py-2 px-4 bg-purple-600 hover:bg-purple-800"
+              onClick={handleNextStep}
+            >
+              Next
+            </button>
           </div>
         );
       case 5:
@@ -116,8 +151,18 @@ const PostProject = () => {
             <div>
               <p>Confrim all the details and submit</p>
             </div>
-            <button onClick={handlePrevStep}>Previous</button>
-            <button onClick={handleSubmit}>Submit</button>
+            <button
+              className="w-1/5 rounded-lg border-2 py-2 px-4 bg-snow-100 hover:bg-purple-200"
+              onClick={handlePrevStep}
+            >
+              Previous
+            </button>
+            <button
+              className="w-1/5 rounded-lg border-2 py-2 px-4 bg-green-600 hover:bg-green-800"
+              onClick={handleSubmit}
+            >
+              Submit
+            </button>
           </div>
         );
       default:
@@ -133,9 +178,11 @@ const PostProject = () => {
 
   return (
     <div className="grid place-items-center">
-      <div className="mt-24 h-[80vh] w-2/3">
-        <h1>Post a Project</h1>
-        <div className="border-b-2 mb-4">
+      <div className="mt-24 h-[80vh] w-2/3 border-2 rounded-lg">
+        <h1 className="font-semibold text-xl text-center py-2">
+          Post a Project
+        </h1>
+        <div className="border-b-2 mb-4 px-4 py-2">
           <div className="w-full h-2 bg-purple-200 rounded-full">
             <div
               className="h-2 bg-purple-700 rounded-full"
@@ -143,7 +190,7 @@ const PostProject = () => {
             ></div>
           </div>
         </div>
-        {renderStepContent()}
+        <div className="py-4 px-4">{renderStepContent()}</div>
       </div>
     </div>
   );
